b373d9dbca6c516f9f3cef618e71f7a472f5122d
Odysseus <arwaterman@gmail.com>
2019-11-13 22:18:34 -0800

    added feature, can now add tags and voting options

--- client/src/pages/PostCreate/PostCreate.js
+++ client/src/pages/PostCreate/PostCreate.js
@@ -1,8 +1,23 @@
 import './PostCreate.css'
-import React from 'react'
+import React, { useState } from 'react'
 // import { Container } from '@material-ui/core'
 
 const PostCreate = () => {
+    const [tag, setTag] = useState('')
+    const [option, setOption] = useState('')
+
+    function handleClick(event) {
+        const target=event.target
+        const text=target.textContent
+        const ref=text.split(' ')[1]
+        const inputTarget=document.getElementById(`${ref}-input`)
+        const appendTarget=document.getElementById(`${ref}s`)
+        const { value } = inputTarget
+        const element=document.createElement('button')
+        element.textContent=value
+        appendTarget.appendChild(element)
+    }
+
     return (
         <section className='post-create'>
             <h1>Create Post</h1>
@@ -13,9 +28,11 @@ const PostCreate = () => {
             <section className='field'>
                 <label>Tags:</label>
                 <section>
-                    <input />
-                    <button>Add tag</button>
-                    <section className='tags'>
+                    <input id='tag-input'
+                    onChange={event => { setTag(event.target.value) }}
+                    value={tag} />
+                    <button onClick={handleClick} >Add tag</button>
+                    <section id='tags'>
                         <button>Gaming</button>
                         <button>2019</button>
                     </section>
@@ -32,9 +49,11 @@ const PostCreate = () => {
             <section className='field'>
                 <label>Voting Options:</label>
                 <section>
-                    <input />
-                    <button>Add option</button>
-                    <section className='tags'>
+                    <input id='option-input'
+                    onChange={event => { setOption(event.target.value) }}
+                    value={option} />
+                    <button onClick={handleClick} >Add option</button>
+                    <section id='options'>
                         <button>League</button>
                         <button>Overwatch</button>
                     </section>
