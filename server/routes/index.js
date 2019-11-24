3059436a66171a9fa77aed3a4b7dba999673e67b
Matthew Park <42101816+matthewpark@users.noreply.github.com>
2019-11-23 21:37:58 -0800

    separated verifyUser into another file

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,17 +10,8 @@ var db = null;
 var accountsCollection = null;
 var postsCollection = null;
 
-// JWT TOKEN VARIABLES
-var jwt = require('jsonwebtoken');
-var privateKey = process.env.PRIVATE_KEY;
-var publicKey = process.env.PUBLIC_KEY;
-
-console.log(privateKey);
-
-// EXPIRATION VARIABLES
-var POST_EXPIRATION_DATE = 30;
-var secondsToExpire = POST_EXPIRATION_DATE*86400;
-
+// JWT TOKEN
+var userAuthentication = require("./verifyUser.js");
 
 // Connect to the MongoDB database only once
 mongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client) {
@@ -38,14 +29,21 @@ mongoClient.connect(uri, { useUnifiedTopology: true }, function(err, client) {
 /**
 Handle GET request for account information.
 */
-router.get("/account/:username", function(req, res, next) {
+router.get("/account/u/:username", function(req, res, next) {
     if(db == null) res.status(400).send({ message : "error connecting to database..." });
     else{
         var requestedUsername = req.params.username;
-        var query = { username : requestedUsername }
+        var authorized = userAuthentication.verifyRequest(req, requestedUsername);
+        var query = { username : requestedUsername };
         var hidden = {projection : {_id : 0,
                                  password : 0,
-                                 emailAddress : 0}}
+                                 emailAddress : 0}};
+
+        // if the user is authorized, do not hide information
+        if(authorized){
+            hidden = {}
+        }
+
         accountsCollection.findOne(query, hidden, function(db_err, db_res) {
                                         if (db_err) res.status(400).send({ message : "invalid request" });
                                         else res.status(201).send({
@@ -57,10 +55,38 @@ router.get("/account/:username", function(req, res, next) {
 });
 
 /**
-CURL COMMAND : curl --data "firstName=John&lastName=Smith&username=johnsmith123&aboutMe=hi-im-john" http://localhost:3001/account
-
+Handle PUT request for account information.
+curl -H "Authorization: Bearer <ACCESS_TOKEN>" -X GET http://localhost:3001/account/u/johnsmith123
 */
-router.post("/account", function(req, res, next) {
+router.put("/account/u/:username", function(req, res, next) {
+    if(db == null) res.status(400).send({ message : "error connecting to database..." });
+    else{
+        var requestedUsername = req.params.username;
+        var authorized = userAuthentication.verifyRequest(req, requestedUsername);
+
+        // if the user is authorized, allow update
+        if(authorized){
+            var query = { username : requestedUsername };
+            var update = {$set: req.body};
+
+            accountsCollection.updateOne(query, update, function(db_err, db_res) {
+                                            if (db_err) res.status(400).send({ message : "invalid request" });
+                                            else res.status(201).send({
+                                                data : db_res,
+                                                message : "success"
+                                            });
+                                      });
+        }
+        else{
+            res.status(404).send({ message : "Unauthorized request" });
+        }
+    }
+});
+
+/**
+CURL COMMAND : curl --data "firstName=John&lastName=Smith&username=johnsmith123&aboutMe=hi-im-john" http://localhost:3001/account
+*/
+router.post("/account/u", function(req, res, next) {
     if(db == null) res.status(400).send({ message : "error connecting to database..." });
     else{
         // Create JSON from POST parameters that contains account information
@@ -75,7 +101,7 @@ router.post("/account", function(req, res, next) {
         accountsCollection.insertOne(entry, function(db_err, db_res) {
                         if (db_err) res.status(400).send({ message : "invalid request" });
                         else res.status(201).send({
-                              data : null,
+                              token : userAuthentication.generateToken(req.body.username),
                               message : "success"
                           });
                   });
@@ -89,6 +115,7 @@ curl --data "tags=gaming&question=my-question&description=mydescription&username
 router.post("/post/id", function(req, res, next) {
     if(db == null) res.status(400).send({ message : "Error connecting to database..." });
     else{
+
         // Create JSON from POST parameters that contains account information
         var entry = {tags : req.body.tags,
                   question : req.body.question,
@@ -139,7 +166,7 @@ router.get("/post/id/:postid", function(req, res, next) {
 /**
 Tests a signin by the user.
 */
-router.post("/signin", function(req, res, next) {
+router.post("/account/signin", function(req, res, next) {
     if(db == null) res.status(400).send({ message : "error connecting to database..." });
     else{
         var signInUsername = req.body.username;
@@ -151,7 +178,7 @@ router.post("/signin", function(req, res, next) {
                     else {
                         if(db_res.password === signInPassword){
                             // Correct password
-                            res.status(200).send({token : generateToken(signInUsername),
+                            res.status(200).send({token : userAuthentication.generateToken(signInUsername),
                                                   message : "success"});
                         }
                         else {
@@ -163,44 +190,20 @@ router.post("/signin", function(req, res, next) {
     }
 });
 
+/*
+Testing verification:
+curl -H "Authorization: Bearer <ACCESS_TOKEN>" --data "username=johnsmith123" http://localhost:3001/verifyuser
+*/
 router.post("/verifyuser", function(req, res, next) {
     var username = req.body.username;
-    console.log(req.headers.authorization);
-    var token = req.headers.authorization.split(' ')[1];
-    //var token = req.body.token;
-    if(verifyToken(username, token)){
+    var authorized = userAuthentication.verifyRequest(req, username);
+    if(authorized){
         res.status(200).send({message : "verified"});
     }
     else{
         res.status(200).send({message : "failed to verify"});
     }
 });
-
-/**
-Returns JWT Token if successful sign in.
-Returns null if invalid credentials.
-*/
-function generateToken(username) {
-    var payload = {
-        account : username
-    };
-    console.log(username);
-    return jwt.sign(payload, privateKey, generateSignAndVerifyOptions(username));
-}
-
-function generateSignAndVerifyOptions(username){
-    return {
-            issuer:  "www.tindigo.com",
-            subject:  "teamindigo54@gmail.com",
-            audience:  username,
-            expiresIn:  "30d",
-            algorithm:  "RS256"
-           };
-}
-
-function verifyToken(username, token) {
-    return jwt.verify(token, publicKey, generateSignAndVerifyOptions(username));
-}
 
 router.get("/welcome", function(req, res, next) {
   res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
