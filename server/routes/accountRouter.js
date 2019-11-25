81b452c5a47a88e9faa71d76e85059ae053f7ca3
Matthew Park <42101816+matthewpark@users.noreply.github.com>
2019-11-24 17:28:33 -0800

    used mongoose instead of mongodb dependencies

--- server/routes/accountRouter.js
+++ server/routes/accountRouter.js
@@ -7,6 +7,10 @@ var mongo = require("../db/index.js");
 // JWT TOKEN
 var userAuthentication = require("./verifyUser.js");
 
+// MODELS
+var AccountModel = require("../models/account.js");
+var PostModel = require("../models/post.js");
+
 /**
 Handle GET request for account information.
 */
@@ -15,22 +19,19 @@ router.get("/account/u/:username", function(req, res, next) {
     else{
         var requestedUsername = req.params.username;
         var query = { username : requestedUsername };
-        var hidden = {projection : {_id : 0,
-                                 password : 0,
-                                 emailAddress : 0}};
+        var databaseCursor = AccountModel.findOne(query);
 
         // if the user is authorized, do not hide information
         if(userAuthentication.verifyRequest(req, requestedUsername)){
-            hidden = {_id : 0}
+            databaseCursor = databaseCursor.select('+password').select('+emailAddress');
         }
+        databaseCursor.exec( function(db_err, db_res) {
+            if (db_err) res.status(400).send({ message : "invalid request" });
+            else res.status(201).send({
+                data : db_res, message : "success"
+            });
+        });
 
-        mongo.accountsCollection.findOne(query, hidden, function(db_err, db_res) {
-                                        if (db_err) res.status(400).send({ message : "invalid request" });
-                                        else res.status(201).send({
-                                            data : db_res,
-                                            message : "success"
-                                        });
-                                  });
     }
 });
 
@@ -48,13 +49,14 @@ router.put("/account/u/:username", function(req, res, next) {
             var query = { username : requestedUsername };
             var update = {$set: req.body};
 
-            mongo.accountsCollection.updateOne(query, update, function(db_err, db_res) {
-                                            if (db_err) res.status(400).send({ message : "invalid request" });
-                                            else res.status(201).send({
-                                                data : db_res,
-                                                message : "success"
-                                            });
-                                      });
+            AccountModel.findOneAndUpdate(query, update, function(db_err, db_res) {
+                  if (db_err) res.status(400).send({ message : "invalid request" });
+                  else res.status(201).send({
+                      data : db_res,
+                      message : "success"
+                  });
+            });
+
         }
         else{
             res.status(404).send({ message : "Unauthorized request" });
@@ -75,15 +77,16 @@ router.post("/account/u", function(req, res, next) {
                   aboutMe : req.body.aboutMe,
                   posts : [],
                   password: req.body.password,
-                  emailAddress: req.body.emailAddress};
+                  emailAddress: req.body.emailAddress,
+                  dateCreated : new Date(Date.now()).toISOString()};
 
-        mongo.accountsCollection.insertOne(entry, function(db_err, db_res) {
-                        if (db_err) res.status(400).send({ message : "invalid request" });
-                        else res.status(201).send({
-                              token : userAuthentication.generateToken(req.body.username),
-                              message : "success"
-                          });
+        new AccountModel(entry).save(function(db_err, db_res) {
+                if (db_err) res.status(400).send({ message : "invalid request" });
+                else res.status(201).send({
+                      token : userAuthentication.generateToken(req.body.username),
+                      message : "success"
                   });
+          });
     }
 });
 
@@ -99,20 +102,20 @@ router.post("/account/signin", function(req, res, next) {
         var signInPassword = req.body.password;
         var query = { username : signInUsername }
 
-        mongo.accountsCollection.findOne(query, function(db_err, db_res) {
-                    if (db_err) res.status(400).send({ message : "invalid request" });
-                    else {
-                        if(db_res.password === signInPassword){
-                            // Correct password
-                            res.status(200).send({token : userAuthentication.generateToken(signInUsername),
-                                                  message : "success"});
-                        }
-                        else {
-                            // Incorrect password
-                            res.status(200).send({message : "incorrect password"});
-                        }
-                    }
-              });
+        AccountModel.findOne(query).select('+password').exec(function(db_err, db_res) {
+              if (db_err) res.status(400).send({ message : "invalid request" });
+              else {
+                  if(db_res.password === signInPassword){
+                      // Correct password
+                      res.status(200).send({token : userAuthentication.generateToken(signInUsername),
+                                            message : "success"});
+                  }
+                  else {
+                      // Incorrect password
+                      res.status(200).send({message : "incorrect password"});
+                  }
+              }
+        });
     }
 });
 
--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -3,49 +3,90 @@
 var express = require("express");
 var router = express.Router();
 var mongo = require("../db/index.js");
+var ObjectId = require('mongodb').ObjectID;
 
 // JWT TOKEN
 var userAuthentication = require("./verifyUser.js");
 
+// MODELS
+var AccountModel = require("../models/account.js");
+var PostModel = require("../models/post.js");
+
+/*
+
+>curl --data "username=johnsmith123&choice=gaming" http://localhost:3001/post/id/5ddb1bc56a7aec499c272d60/vote
+*/
+router.post("/post/id/:postid/vote", function(req, res, next) {
+    if(mongo.database == null) res.status(400).send({ message : "Error connecting to database..." });
+    else{
+        var requestedPostID = req.params.postid;
+        var username = req.body.username;
+        var choice = req.body.choice;
+        var query = { _id : ObjectId(requestedPostID) };
+        PostModel.findById(requestedPostID, function(db_err, db_res) {
+              if (db_err) res.status(400).send({ message : "Invalid request" });
+              else {
+                    db_res.vote(username, choice);
+                    db_res.save(function(db_err, db_res) {
+                          if (db_err) res.status(400).send({ message : "Invalid request" });
+                          else {
+                                res.status(201).send({
+                                      data : db_res,
+                                      message : "success"
+                                });
+                          }
+
+                    });
+
+              }
+        });
+
+    }
+});
+
 /**
 Create post using POST method
-curl --data "tags=gaming&question=my-question&description=mydescription&username=johnsmith123" http://localhost:3001/post/id
+curl --data "tags=2019&username=johnsmith123&choices=gaming&choices=league" http://localhost:3001/post/id
 */
 router.post("/post/id", function(req, res, next) {
     if(mongo.database == null) res.status(400).send({ message : "Error connecting to database..." });
     else{
         var username = req.body.username;
 
-        if(userAuthentication.verifyRequest(req, username)){
+        //if(userAuthentication.verifyRequest(req, username)){
             // Create JSON from POST parameters that contains account information
             var entry = {tags : req.body.tags,
                       question : req.body.question,
                       description : req.body.description,
                       username : req.body.username,
-                      choices : [],
-                      choiceStatus : [],
-                      votes : []};
+                      choices : req.body.choices,
+                      userVotes : [],
+                      dateCreated : new Date(Date.now()).toISOString()};
 
             // Add the post to the post_info collection
-            mongo.postsCollection.insertOne(entry, function(db_err, db_res) {
-                if (db_err) res.status(400).send({ message : "Invalid request" });
-                else {
-                    var postID = entry._id;
-                    var query = {username : req.body.username};
-                    var update = {$addToSet: { "posts" : postID}};
-                    accountsCollection.updateOne(query, update, function(accounts_db_err, accounts_db_res) {
-                                    if (accounts_db_err) res.status(400).send({ message : "Invalid request" });
-                                    else res.status(200).send({
-                                        data : {_id : postID},
-                                        message : "success"
-                                    });
-                              });
-                }
-            });
+            newPost = new PostModel(entry);
+            newPost.init_choices();
+            newPost.save(function(db_err, db_res) {
+                      if (db_err) res.status(400).send({ message : "Invalid request" });
+                      else {
+                          var postID = db_res._id;
+                          var query = {username : req.body.username};
+                          var update = {$addToSet: { "posts" : postID.toString()}};
+
+                          AccountModel.findOneAndUpdate(query, update, function(accounts_db_err, accounts_db_res) {
+                                         if (accounts_db_err) res.status(400).send({ message : "Invalid request" });
+                                         else res.status(200).send({
+                                             data : {_id : postID},
+                                             message : "success"
+                                         });
+                                   });
+                      }
+                  });
+            /*
         }
         else{
             res.status(404).send({ message : "Unauthorized request" });
-        }
+        }*/
     }
 });
 
@@ -57,13 +98,48 @@ router.get("/post/id/:postid", function(req, res, next) {
     else{
         var requestedPostID = req.params.postid;
         var query = { _id : requestedPostID };
-        mongo.postsCollection.findOne(query, function(db_err, db_res) {
-                                        if (db_err) res.status(400).send({ message : "Invalid request" });
-                                        else res.status(201).send({
-                                              data : db_res,
-                                              message : "success"
-                                          });
-                                  });
+
+        PostModel.findOne(query, function(db_err, db_res) {
+            if (db_err) res.status(400).send({ message : "Invalid request" });
+            else res.status(201).send({
+                 data : db_res,
+                 message : "success"
+             });
+        });
+    }
+});
+
+
+router.get("/post/search", function(req, res, next) {
+    if(mongo.database == null) res.status(400).send({ message : "Error connecting to database..." });
+    else{
+        var searchTag = req.query.tag;
+        var query = {tags: { "$in": searchTag  }};
+        var beginIndex = 0;
+        var limit = 10;
+
+        if(req.query.begIndex){
+            beginIndex = parseInt(req.query.begIndex);
+            if(isNaN(beginIndex) || beginIndex < 0){
+                res.status(400).send({ message : "Begin index is not an integer." });
+            }
+        }
+        if(req.query.limit) {
+            limit = parseInt(req.query.limit);
+            if(isNaN(limit) || limit < 0){
+                res.status(400).send({ message : "Limit is not an integer." });
+            }
+        }
+
+        PostModel.find(query).skip(beginIndex).limit(limit).sort({dateCreated : 1}).exec(function(db_err, db_res) {
+              if (db_err) res.status(400).send({ message : "Invalid request" });
+              else res.status(201).send({
+                    data : db_res,
+                    size : db_res.length,
+                    message : "success"
+                });
+        });
+
     }
 });
 
