dd2b056fd528525276fce86fbee68c9827a5e14c
Odysseus <arwaterman@gmail.com>
2019-12-06 16:34:48 -0800

    removed commented code

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,9 +8,6 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
-// import AccountCircle from '@material-ui/icons/AccountCircle';
-// import MailIcon from '@material-ui/icons/Mail';
-// import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
