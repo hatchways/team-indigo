39025d762b3d273ce060187c3cac7129c1535bcc
Odysseus <arwaterman@gmail.com>
2019-12-09 14:27:34 -0800

    user home page now requires user to log into application

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 
 import { Container, Grid, Avatar } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
@@ -13,8 +14,21 @@ const useStyles = makeStyles(theme => ({
     },
   }));
 
-function HomePage({props}) {
+function HomePage(props) {
     const classes = useStyles()
+
+    const redirectToTarget = (path) => {
+        props.history.push(path);
+    }
+
+    let username
+    if (window.sessionStorage.username) {
+        username=window.sessionStorage.username
+    }
+    else {
+        redirectToTarget('/account')
+    }
+
     return (
         <Container>
             <h1>Welcome to Tindigo!</h1>
@@ -27,7 +41,7 @@ function HomePage({props}) {
                 </Grid>
 
                 <Grid item xs={8}>
-                    <h2>Name McNamerson</h2>
+                    <h2>{username}</h2>
                     <h3>About me</h3>
                     <p>Fugiat aute dolore dolore aliquip nulla fugiat laboris esse. Exercitation occaecat nulla fugiat magna occaecat ut sit fugiat culpa eu occaecat. Velit qui velit cupidatat anim tempor cillum aliqua ut magna velit ad. Cillum enim et deserunt nostrud ut eiusmod qui elit. Nostrud ea Lorem laborum ea amet elit minim.</p>
                 </Grid>
@@ -41,4 +55,4 @@ function HomePage({props}) {
     );
 }
 
-export default HomePage;
+export default withRouter(HomePage);
