bf4ca0dba306110a4bbaa080cad032d7c1e773b1
Odysseus <arwaterman@gmail.com>
2019-12-09 13:33:56 -0800

    added API function postDataWithToken

--- client/src/utilities/API.js
+++ client/src/utilities/API.js
@@ -1,4 +1,22 @@
 export const postData = async (url, data) => {
+  const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrer: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+  })
+    
+  return await response.json() // parses JSON response into native JavaScript objects
+}
+
+export const postDataWithToken = async (url, data, token) => {
     const response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
@@ -10,7 +28,8 @@ export const postData = async (url, data) => {
         },
         redirect: 'follow', // manual, *follow, error
         referrer: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+        Authorization : `Bearer ${token}`
     })
       
     return await response.json() // parses JSON response into native JavaScript objects
