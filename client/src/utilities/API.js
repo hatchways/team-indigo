92c7ebe33d5bd060c01725eed33fff19defed53c
Odysseus <arwaterman@gmail.com>
2019-12-10 13:46:37 -0800

    refactored getData and postData API calls

--- client/src/utilities/API.js
+++ client/src/utilities/API.js
@@ -1,4 +1,4 @@
-export const postData = async (url, data) => {
+export const postData = async (url, data, token='') => {
   const response = await fetch(url, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
@@ -10,32 +10,14 @@ export const postData = async (url, data) => {
       },
       redirect: 'follow', // manual, *follow, error
       referrer: 'no-referrer', // no-referrer, *client
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+      Authorization : `Bearer ${token}`
   })
     
   return await response.json() // parses JSON response into native JavaScript objects
 }
 
-export const postDataWithToken = async (url, data, token) => {
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrer: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-        Authorization : `Bearer ${token}`
-    })
-      
-    return await response.json() // parses JSON response into native JavaScript objects
-}
-
-export const getData = async (url, token) => {
+export const getData = async (url, token='') => {
   const response = await fetch(url, {
     method: 'GET',
     mode: 'cors',
