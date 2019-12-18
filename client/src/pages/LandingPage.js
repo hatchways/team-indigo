3aa20c72e7cdbb1c1d0099df1c3e801638631770
Odysseus <arwaterman@gmail.com>
2019-12-17 19:21:07 -0800

    attempting to retrieve posts from backend using post id

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -5,11 +5,38 @@ import { Link } from 'react-router-dom'
 
 import { Container, Grid } from '@material-ui/core';
 
+import { getData } from '../utilities/API'
+
 function LandingPage(props) {
 
-    let username
+    let username, token
     if (window.sessionStorage.username) {
         username = window.sessionStorage.username
+        token = window.sessionStorage.token
+    }
+
+    const getPosts = async () => {
+        const response = await getData(`/account/u/${username}`, token)
+        console.log(response)
+
+        const postIds = response.data.posts
+        // create an array of promises
+        const promises = []
+
+        for (let i=0; i < postIds.length; i++) {
+            const id = postIds[i]
+            promises.push(
+                getData(`post/id/${id}`, token)
+            )
+        }
+
+        const responses = await Promise.all(promises)
+        console.log(responses)
+    }
+
+    let posts
+    if (username) {
+        posts = getPosts()
     }
 
     return (
@@ -28,15 +55,22 @@ function LandingPage(props) {
                     <Grid item xs={12}>
                         <h1>Your Posts</h1>
                     </Grid>
-                    {/* map each post */}
-                    <Grid item xs={12}>
-                        {/* return some question component  */}
-                        <h2>Question goes here?</h2>
-                        <p>yes - 63%</p>
-                        <p>no - 47%</p>
-                    </Grid>
-                </Grid> ) : ''
+                
+                    {/* {posts.map(post => {
+                        return (
+                            <Grid item xs={12}>
+                                <h2>{post.question}</h2>
+
+                                {post.choices.map(choice => {
+                                    return <p>{choice}</p>
+                                })}
+
+                            </Grid>
+                        )}
+                    )}*/}
+                </Grid> ) : '' 
             }
+            
             <Grid container>
                 <Grid item xs={12}>
                     <h1>Popular Posts</h1>
