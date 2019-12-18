5c01f3d80610b1f7d76ea7e1f7f318b6dc57591e
Odysseus <arwaterman@gmail.com>
2019-12-17 17:42:42 -0800

    added JSON.stringify call into postData call

--- client/src/utilities/API.js
+++ client/src/utilities/API.js
@@ -10,7 +10,7 @@ export const postData = async (url, data, token='') => {
       },
       redirect: 'follow', // manual, *follow, error
       referrer: 'no-referrer', // no-referrer, *client
-      body: data, // body data type must match "Content-Type" header
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
   })
     
   return await response.json() // parses JSON response into native JavaScript objects
